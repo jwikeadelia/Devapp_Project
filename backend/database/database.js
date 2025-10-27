1771ebc8d44d259e79c3ec0438a92ddf40991e3c
Doni Charles Hutagaol <67886917+donnycharles88@users.noreply.github.com>
2025-10-27 14:43:39 +0700

    mengubah host.docker.internal di database.js

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,7 +1,8 @@
 import { Sequelize, DataTypes } from "sequelize";
 
 const sequelize = new Sequelize("crud_task", "root", "", {
-  host: "localhost",
+  host: "host.docker.internal",
+  // host: "localhost",
   dialect: "mysql",
 });
 
